0ba48a56bf765513a1c19b334b284da86fbf408f
Anandck07 <kumbaranand777@gmail.com>
2026-03-22 22:08:33 +0530

    created frontend UI

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 }));
 app.use(express.json());
